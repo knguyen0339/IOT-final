2c4d1d239a7726ade06aeb60388b2e949ff6621a
JustAnPearl <dle1322@sdsu.edu>
2023-04-19 14:24:16 -0700

    Set up button sensor, OLED display

---
+++ final project/src/test.cpp
@@ -0,0 +1,81 @@
+// Source: https://randomnerdtutorials.com/esp32-i2c-communication-arduino-ide/
+#include <Arduino.h>
+#include <Wire.h>
+#include <TFT_eSPI.h>
+#include "Adafruit_CAP1188.h"
+
+// Configure LED pins as outputs. 
+#define RED_PIN 33
+#define YELLOW_PIN 32
+#define GREEN_PIN 39
+#define WHITE_PIN 38
+
+#define BUZZER 17
+int ledState = LOW;
+bool ledFlash = false;
+
+int count = 0; 
+
+TFT_eSPI tft = TFT_eSPI(); 
+
+
+Adafruit_CAP1188 cap = Adafruit_CAP1188();
+
+void setup() {
+  tft.init();
+  tft.setRotation(1);
+  tft.width();
+  tft.height();
+  tft.setTextSize(3);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE);
+
+  pinMode(RED_PIN, OUTPUT); 
+  pinMode(YELLOW_PIN, OUTPUT); 
+  pinMode(GREEN_PIN, OUTPUT);
+
+  Wire.begin();
+  Serial.begin(9600);
+
+  Serial.println("CAP1188 test!");
+  if (!cap.begin()) {
+    Serial.println("CAP1188 not found");
+    while (1);
+  }
+  Serial.println("CAP1188 found!");
+}
+
+ 
+void loop() {
+    // OLED display when the button sensor is pressed
+    uint8_t touched = cap.touched();
+    if (touched != 0) {
+        Serial.println("Botton pressed");
+        count = 0;
+        ledState = HIGH;
+        ledFlash = true;
+        tft.drawString("Bell's ringing", 10, 10, 2);
+    }
+
+    // Control led lights
+    digitalWrite(RED_PIN, ledState); 
+    digitalWrite(YELLOW_PIN, ledState); 
+    digitalWrite(GREEN_PIN, ledState);
+    if (ledFlash == true)
+    {
+      if (ledState == HIGH)
+        ledState = LOW;
+      else
+        ledState = HIGH;
+    }
+    
+    // Turn off all alerts 5s after the button is pressed
+    count += 200;
+    if (count >= 5000){
+      count = 0;
+      tft.fillScreen(TFT_BLACK);
+      ledState = LOW;
+      ledFlash = false;
+    }
+    delay(200);
+}
